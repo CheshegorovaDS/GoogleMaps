d0370898fd8f250066e0b932517a6af5dad22131
dcheshegorova <peschink@mail.ru>
2020-09-01 12:23:45 +0600

    Add moving in map and methods of creating circle, polyline and polygon. Show infomation about different object in map.

--- app/src/main/java/com/novikova/darya/MapFragment.kt
+++ app/src/main/java/com/novikova/darya/MapFragment.kt
@@ -1,6 +1,7 @@
 package com.novikova.darya
 
 import android.Manifest
+import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,19 +10,15 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.*
+import kotlinx.android.synthetic.main.fragment_maps.*
 import pub.devrel.easypermissions.EasyPermissions
 
-
+const val OFFSET = 300f
 class MapFragment: Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
-    GoogleMap.OnMyLocationClickListener, GoogleMap.OnMarkerClickListener {
+    GoogleMap.OnMyLocationClickListener, GoogleMap.OnMarkerClickListener,
+    GoogleMap.OnPolylineClickListener, GoogleMap.OnPoiClickListener {
 
     private val resLayout = R.layout.fragment_maps
 
@@ -43,13 +40,35 @@ class MapFragment: Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonC
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-//        googleMap.isMyLocationEnabled = (activity as MainActivity).mLocationPermissionsGranted
         googleMap.isMyLocationEnabled = EasyPermissions
             .hasPermissions(activity!!.applicationContext, Manifest.permission.ACCESS_FINE_LOCATION)
+//        вид карты
+        googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+//        определение местоположения
         googleMap.setOnMyLocationButtonClickListener(this)
         googleMap.setOnMyLocationClickListener(this)
 
+        creatingMarker(googleMap)
 
+        creatingPolyline(googleMap)
+
+        creatingPolygon(googleMap)
+
+        creatingCircle(googleMap)
+
+        buttonClickListener(googleMap)
+
+        googleMap.setOnPoiClickListener(this)
+
+//         появление кнопок приближения и удаления
+        googleMap.uiSettings.isZoomControlsEnabled = true
+
+//        отключение ссылки на карты Google Maps
+        googleMap.uiSettings.isMapToolbarEnabled = false
+
+    }
+
+    private fun creatingMarker(googleMap: GoogleMap) {
         val marker = LatLng(0.0,0.0)
         googleMap.addMarker(MarkerOptions().position(marker).title("Zero coordinate"))
 
@@ -78,9 +97,83 @@ class MapFragment: Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonC
                     )
                 )
         )
-       googleMap.setOnMarkerClickListener(this)
+        googleMap.setOnMarkerClickListener(this)
 
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(bridgeOmsk))
+    }
+
+    private fun creatingPolyline(googleMap: GoogleMap) {
+        val polyline = googleMap.addPolyline(
+            PolylineOptions()
+                .clickable(true)
+                .add(
+                    LatLng(-35.016, 143.321),
+                    LatLng(-34.747, 145.592),
+                    LatLng(-34.364, 147.891),
+                    LatLng(-33.501, 150.217),
+                    LatLng(-32.306, 149.248),
+                    LatLng(-32.491, 147.309)
+                )
+        )
+//        style
+
+//        закргуленные концы
+        polyline.startCap = RoundCap()
+        polyline.endCap = RoundCap()
+
+//        цвет
+        polyline.color = Color.CYAN
+
+        polyline.isGeodesic = true
+
+        googleMap.setOnPolylineClickListener(this)
+    }
+
+    private fun  creatingPolygon(googleMap: GoogleMap) {
+        val polygon = googleMap.addPolygon(
+            PolygonOptions()
+                .clickable(true)
+                .add(
+                    LatLng(-27.457, 153.040),
+                    LatLng(-33.852, 151.211),
+                    LatLng(-37.813, 144.962),
+                    LatLng(-34.928, 138.599)
+                )
+        )
+
+    }
+
+    private fun creatingCircle(googleMap: GoogleMap) {
+        val circle =  CircleOptions()
+            .center(LatLng(0.0, 15.0))
+            .radius(50000.0)
+            .strokeColor(Color.GREEN)
+            .strokeWidth(5f)
+            .fillColor(0x5500ff00)
+        googleMap.addCircle(circle)
+    }
+
+    private fun buttonClickListener(googleMap: GoogleMap) {
+        //            перемещение камеры на указанное кол-во пикселов
+        top.setOnClickListener {
+            val c= CameraUpdateFactory.scrollBy(0f,-OFFSET)
+            googleMap.animateCamera(c)
+        }
+
+        down.setOnClickListener {
+            val c= CameraUpdateFactory.scrollBy(0f, OFFSET)
+            googleMap.animateCamera(c)
+        }
+
+        right.setOnClickListener {
+            val c= CameraUpdateFactory.scrollBy(OFFSET,0f)
+            googleMap.animateCamera(c)
+        }
+
+        left.setOnClickListener {
+            val c= CameraUpdateFactory.scrollBy(-OFFSET,0f)
+            googleMap.animateCamera(c)
+        }
     }
 
     override fun onMyLocationButtonClick(): Boolean {
@@ -98,6 +191,16 @@ class MapFragment: Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonC
     override fun onMarkerClick(marker: Marker): Boolean {
         Toast.makeText(context,"Click marker ${marker.title}", Toast.LENGTH_SHORT).show()
         return false
+    }
+
+    override fun onPolylineClick(polyline: Polyline?) {
+        Toast.makeText(context, "Click Polyline", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPoiClick(poi: PointOfInterest?) {
+        val text = "Clicked:  ${poi?.name } \nPlace ID: ${poi?.placeId} \nLatitude: ${poi?.latLng?.latitude} \nLongitude: ${poi?.latLng?.longitude}"
+        Toast.makeText(context, text ,
+            Toast.LENGTH_SHORT).show()
     }
 
 
